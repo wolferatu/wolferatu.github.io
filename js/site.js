03f4249e7b1d6ee92f0cbedcd3c888fa1c9a21f4
wolferatu <wolferatu@protonmail.com>
2021-07-06 00:13:17 -0700

    Remove load

--- js/site.js
+++ js/site.js
@@ -48,20 +48,4 @@ $(document).ready(function () {
 });
 
 
-/* Preloader and animations */
-$(window).load(function () { // makes sure the whole site is loaded
-	$('#status').fadeOut(); // will first fade out the loading animation
-	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-	$('body').delay(350).css({'overflow-y': 'visible'});
 
-	/* WOW Elements */
-	if (typeof WOW == 'function') {
-		new WOW().init();
-	}
-
-	/* Parallax Effects */
-	if (!!$.prototype.enllax) {
-		$(window).enllax();
-	}
-
-});
